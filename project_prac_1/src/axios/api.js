a51c569f2ed8f659f9f31c0b59b90d7f8748a11a
Mindfulness-en-toute-Tranquillite <tvghoy@gmail.com>
2023-03-21 14:03:32 +0900

    axios interceptor 오류 확인.

--- project_prac_1/src/axios/api.js
+++ project_prac_1/src/axios/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL : process.env.REACT_APP_URL,
+    baseURL: process.env.REACT_APP_URL,
+    timeout: 1
 });
 
 instance.interceptors.request.use(
