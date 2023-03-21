26cd7d89163e9b987a54ceae644c18b201d8a4af
Mindfulness-en-toute-Tranquillite <tvghoy@gmail.com>
2023-03-21 14:01:36 +0900

    axios interceptor 적용.

--- project_prac_1/src/axios/api.js
+++ project_prac_1/src/axios/api.js
@@ -4,4 +4,31 @@ const instance = axios.create({
     baseURL : process.env.REACT_APP_URL,
 });
 
+instance.interceptors.request.use(
+    // 요청을 보내기 전 수행되는 함수
+    function (config) {
+        console.log("인터셉터 요청 성공!");
+        return config;
+    },
+    // 오류 요청을 보내기 전 수행되는 함수
+    function(error){
+        console.log("인터셉터 요청 오류!")
+        return Promise.recject(error);
+    },
+)
+
+instance.interceptors.response.use(
+    //응답을 내보내기 전 수행되는 함수
+    function(response){
+        console.log("인터셉터 응답 받았습니다!")
+    },
+
+    //오류 응답을 내보내기 전 수행되는 함수
+    function(error){
+        console.log("인터셉터 응답 오류 발생")
+        return Promise.reject(error)
+    }
+);
+
+
 export default instance;
